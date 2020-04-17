dddac768020689574c16325fd9cff2c61d4d6261
Ayush Vora <ayushvora10@gmail.com>
2020-04-17 20:09:40 +0530

    unneeded serialization vul done, plus some minor changes in others

--- src/main/java/com/il/securecode/AppResource.java
+++ src/main/java/com/il/securecode/AppResource.java
@@ -54,17 +54,17 @@ public final class AppResource {
             "  }\n" +
 
             "  @Test\n" +
-            "  void checkSuccessfulLogin(){\n" +
+            "  void checkSuccessfulLogin() {\n" +
             "    assertEquals(\"login successful\", login(\"existing_user\", \"correct_password\"));\n" +
             "  }\n" +
 
             "  @Test\n" +
-            "  void checkUserDoesNotExist(){\n" +
+            "  void checkUserDoesNotExist() {\n" +
             "    assertEquals(\"invalid username or password\", login(\"not_existing_user\", \"doesnt_matter\"));\n" +
             "  }\n" +
 
             "  @Test\n" +
-            "  void checkPasswordNotMatched(){\n" +
+            "  void checkPasswordNotMatched() {\n" +
             "    assertEquals(\"invalid username or password\", login(\"existing_user\", \"wrong_password\"));\n" +
             "  }\n" +
             "}\n"
@@ -118,13 +118,14 @@ public final class AppResource {
             "    }\n " +
             "  }\n" +
             
-            "  boolean fn(String xmlString){\n" +
+            "  boolean fn(String xmlString) {\n" +
             code +
             "    return factory.fSet && reader.fSet;\n" +
             "  }\n" +
 
+            // this, unfortunately, doesn't ensure that the factory instance's feature is set BEFORE it is used to instantiate the parser
             "  @Test\n" +
-            "  void checkFeatureFlagsSet(){\n" +
+            "  void checkFeatureFlagsSet() {\n" +
             "    assertTrue(fn(\"<somexml></somexml>\"));\n" +
             "  }\n" +
             "}\n"
@@ -160,11 +161,48 @@ public final class AppResource {
             code +
 
             "  @Test\n" +
-            "  void checkEncoding(){\n" +
+            "  void checkEncoding() {\n" +
             "    assertEquals(\n" +
             "      \"<div>Here are the product's features: feature1, feature2, &lt;script&gt;some-script&lt;/script&gt;</div>\",\n" +
             "      getHtmlDiv(\"feature1, feature2, <script>some-script</script>\")\n" +
             "    );\n" +
+            "  }\n" +
+            "}\n"
+        ).create().get();
+
+        final LauncherDiscoveryRequest request = 
+            LauncherDiscoveryRequestBuilder.request().selectors(selectClass(s.getClass())).build();
+
+        final Launcher launcher = LauncherFactory.create();
+        final SummaryGeneratingListener listener = new SummaryGeneratingListener();
+        launcher.registerTestExecutionListeners(listener);
+        launcher.execute(request);
+
+        return listener.getSummary().getTestsSucceededCount() == 1 ? "OK" : "NO";
+    }
+
+    @Path("/vul5")
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String vul5(final String code) {
+        final long shouldBeOkayForNow = System.currentTimeMillis();
+
+        final Object s = Reflect.compile(
+            "com.il.securecode.NewCompileTest" + shouldBeOkayForNow,
+            "package com.il.securecode;\n" +
+
+            "import static org.junit.jupiter.api.Assertions.assertEquals;\n" +
+            "import org.junit.jupiter.api.Test;\n" +
+
+            "class NewCompileTest" + shouldBeOkayForNow +"{\n" +
+
+            code +
+
+            "  @Test\n" +
+            "  void checkGetCountryCookie() {\n" +
+            "    final NewCompileTest" + shouldBeOkayForNow + " x = new NewCompileTest" + shouldBeOkayForNow + "();\n" +
+            "    x.country=\"IN\";\n" +
+            "    assertEquals(\"country=IN\", x.getCountryCookie());\n" +
             "  }\n" +
             "}\n"
         ).create().get();
